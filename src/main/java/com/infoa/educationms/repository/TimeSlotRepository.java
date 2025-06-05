21da831307e0c0eb58eced53a4df2cb55a524420
LCG-UO <63496898+LCG-UO@users.noreply.github.com>
2025-06-05 15:28:04 +0800

    Add files via upload

--- src/main/java/com/infoa/educationms/repository/TimeSlotRepository.java
+++ src/main/java/com/infoa/educationms/repository/TimeSlotRepository.java
@@ -4,6 +4,9 @@ import com.infoa.educationms.entities.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, Integer> {
+    List<TimeSlot> findAllByIds(List<Integer> timeSlotIds);
 }
